3c0d34da4eb50912743fa0163afb86714dc0f5ff
shah-jamal <jamalweb007@gmail.com>
2023-04-19 14:41:52 +0600

    country detail add

--- js/countries.js
+++ js/countries.js
@@ -12,13 +12,28 @@ const displayCountries = countries => {
         console.log(country.name);
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name.common;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        div.innerHTML = `
+            <h3>${country.name.common}</h3>
+            <p>${country.capital}</p>
+            <button onclick="loadCountryByName('${country.name.common}')">Details</button>
+        `;
         countriesDiv.appendChild(div);
     });
 
+}
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]));
+}
+
+const displayCountryDetail = country => {
+    const countryDiv = document.getElementById('country-detail')
+    countryDiv.innerHTML = `
+        <h5>${country.name.common}</h5>
+        <p>Population: ${country.population}</p>
+        <img width="150px" src="${country.flags.png}">
+    `
 }
